b08468fdef5e77865fc6305129b0e2d887f0a33e
Andres2295 <Andres2295@users.noreply.github.com>
2020-03-23 02:01:39 -0400

    Master update

---
+++ Code Examples/C#/if-else-example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace else_if_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Are you new to C#?\nyes/no");
+            string enteredVal = Console.ReadLine().ToString();
+
+            if(enteredVal == "yes"){
+                Console.WriteLine("Hello Welcome to C#!\n");
+            }
+            else{
+                Console.WriteLine("Remember that practice makes perfect so... Practice , Practice, Practice!\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
